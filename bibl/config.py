a12b3857c332b917d6695dc9bdb920422ac0247d
Arne Van Den Kerchove <arne@vandenkerchove.com>
2020-11-05 13:53:21 +0100

    Rework config

--- bibl/config.py
+++ bibl/config.py
@@ -2,46 +2,43 @@ import pkg_resources
 import yaml
 
 _config = dict()
+_read_default = False
 
-_DEFAULT_CONFIG_FILE = '.bib.yml'
+_DEFAULT_CONFIG_FILE = '.bibl.yml'
 
 
 def get_config():
-    global _config
-    if not _config:
+    if not _read_default:
         _load_default_config()
     return _config
 
 
-def set_config(key, value):
-    global _config
-    if not _config:
-        _load_default_config()
+def set_config(key, value, default=False):
     if not value is None:
-        _config[key] = value
-        if key in {'select', 'ignore'} and _config[key] is None:
-            _config[key] = []
-        _validate_config(_config)
+        if default:
+            _config.setdefault(key, value)
+        else:
+            _config[key] = value
+        _validate_and_clean_config(_config)
 
 
 def load_config_file(file):
     with open(file) as config_file:
         config = yaml.load(config_file, Loader=yaml.FullLoader)
-        _load_config(config)
-
-
-def _load_config(config):
-    for k, v in config.items():
-        set_config(k, v)
+        for k, v in config.items():
+            set_config(k, v)
 
 
 def _load_default_config():
-    with open(pkg_resources.resource_filename(_DEFAULT_CONFIG_FILE, "resourcefile")) as default_config_file:
+    global _read_default
+    _read_default = True
+    with open(pkg_resources.resource_filename(__name__, _DEFAULT_CONFIG_FILE)) as default_config_file:
         default_config = yaml.load(default_config_file, Loader=yaml.FullLoader)
-        _load_config(default_config)
+        for k, v in default_config.items():
+            set_config(k, v, default=True)
 
 
-def _validate_config(config):
+def _validate_and_clean_config(config):
     if 'select' in config and 'ignore' in config and config['select'] and config['ignore']:
         raise ValueError(
             "Configuration cannot contain both included and selected and ignored rules. Use either include or exclude to select"
