00fad30f98b248daeeef3b2bed9d6afb60ea219d
Arne Van Den Kerchove <arne@vandenkerchove.com>
2020-11-15 18:11:32 +0100

    Documented text rules

---
+++ bibl/rules/__init__.py
@@ -0,0 +1,6 @@
+"""Package that contains modules with linter rules.
+
+Modules in this package will automatically be imported when loading rules.
+If these modules contain linter rule functions registered with
+`@register_entry_rule`, they will be added to the linter.
+"""
--- bibl/rules/text_rules.py
+++ bibl/rules/text_rules.py
@@ -1,3 +1,5 @@
+"""Linter rules checking the text lay-out of the bibliography file."""
+
 import re
 
 from bibl.config import get_config
@@ -6,26 +8,76 @@ from bibl.rule import register_text_rule
 
 @register_text_rule('T00', 'Non-ascii character')
 def ascii_chars(line_number, line, text):
+    """Raise a linter warning when a line contains a non-ascii character.
+
+    :param line_number: The number of the current line in the bibliography
+    :param line: The content of the current line in the bibliography
+    :param text: The entire bibliography
+    :return: True if all characters in the current line are ascii
+    characters, False otherwise.
+    """
     return line.isascii()
 
 
-@register_text_rule('T01', 'Non-standard whitespace at beginning of line (indents should be {} spaces)'.format(
-    get_config()['indent_spaces']))
+@register_text_rule('T01',
+                    'Non-standard whitespace at beginning of line (indents '
+                    'should be {} spaces)'
+                    .format(get_config()['indent_spaces']))
 def trailing_whitespace(line_number, line, text):
-    regex = re.compile(r'(^ {' + str(get_config()['indent_spaces']) + r'}\S)|(^\S)|(^$)')
+    """Raise a linter warning when a line starts with a non-standard indent.
+
+    Valid indents are space characters in multiples of the indent defined in
+    the configuration with the `indent_spaces`.
+    This rule also raises a warning when a line starts with a whitespace
+    character that is not a space.
+
+    Invalid trailing whitespace characters are:
+    * A tab character
+    * A vertical tab character
+    * A form feed character
+
+    :param line_number: The number of the current line in the bibliography
+    :param line: The content of the current line in the bibliography
+    :param text: The entire bibliography
+    :return: True if the current line does not start
+    """
+    regex = re.compile(
+        r'(^ {' + str(get_config()['indent_spaces']) + r'}\S)|(^\S)|(^$)')
     return bool(regex.match(line))
 
 
 @register_text_rule('T02', 'Whitespace at end of line')
 def ending_whitespace(line_number, line, text):
+    """Raise a linter warning when a line ends in a whitespace character.
+
+    Whitespace characters are:
+    * A space character
+    * A tab character
+    * A vertical tab character
+    * A form feed character
+
+    :param line_number: The number of the current line in the bibliography
+    :param line: The content of the current line in the bibliography
+    :param text: The entire bibliography
+    :return: True if the current line des not end in whitespace,
+    False otherwise.
+    """
     regex = re.compile(r'(^.*\S$)|(^$)')
     return bool(regex.match(line))
 
 
-@register_text_rule('T03', 'Line length exceeds {} characters'.format(get_config()['max_line_length']))
+@register_text_rule('T03', 'Line length exceeds {} characters'.format(
+    get_config()['max_line_length']))
 def line_length(line_number, line, text):
-    return len(line) <= get_config()['max_line_length']
+    """Raise a linter warning when a maximum line length is exceeded.
 
-# TODO: invalid syntax
-# TODO: preamble first line
-# TODO: string directly after preamble
+     The maximum line length is defined in the configuration with the
+     `max_line_length` setting.
+
+    :param line_number: The number of the current line in the bibliography
+    :param line: The content of the current line in the bibliography
+    :param text: The entire bibliography
+    :return: True if the current line length is smaller or equal to the
+    maximum number of characthers, False otherwise
+    """
+    return len(line) <= get_config()['max_line_length']
